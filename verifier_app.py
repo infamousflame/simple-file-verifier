a60f439f1a4320ca93ed23ab180d3d70ed3c9a5c
infamousflame <67448147+infamousflame@users.noreply.github.com>
2024-06-19 16:38:08 +1200

    Create verifier_app.py
    Created a module to make a GUI and call the other GUI-related modules.

---
+++ verifier_app.py
@@ -0,0 +1,62 @@
+"""Provides the `VerifierApp` class."""
+
+__all__ = ['VerifierApp']
+
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+from file_choice_pane import FileChoicePane
+from hash_choice_pane import HashChoicePane
+from hashing import hasher
+
+LEFT_SIDE_WIDTH = 150
+BOTTOM_SIDE_HEIGHT = 150
+
+
+class VerifierApp(App):
+    """The actual all class."""
+
+    def build(self) -> GridLayout:
+        """Build the app.
+
+        Returns:
+            GridLayout: The window's layout.
+        """
+        self.window: GridLayout = GridLayout(rows = 2, cols = 2)
+        self.title = 'File Verifier'
+
+        self.hash_choice_pane: HashChoicePane = HashChoicePane(
+            size_hint_x = None,
+            width = LEFT_SIDE_WIDTH
+        )
+        self.window.add_widget(self.hash_choice_pane)
+
+        self.file_choice_pane: FileChoicePane = FileChoicePane()
+        self.window.add_widget(self.file_choice_pane)
+
+        self.calculate_button: Button = Button(
+            text = 'COMPUTE SUM',
+            size_hint = (None, None),
+            size = (LEFT_SIDE_WIDTH, BOTTOM_SIDE_HEIGHT)
+        )
+        self.calculate_button.bind(on_release = (
+            lambda instance:
+                self.update_digest_label()
+        ))
+        self.window.add_widget(self.calculate_button)
+
+        self.hash_sum_output: TextInput = TextInput(
+            text = 'Computed hash sum',
+            size_hint_y = None,
+            height = BOTTOM_SIDE_HEIGHT
+        )
+        self.window.add_widget(self.hash_sum_output)
+
+        return self.window
+
+
+    def update_digest_label(self) -> None:
+        """Updates the hash digest label."""
+        self.hash_sum_output.text = hasher.compute_digest()
